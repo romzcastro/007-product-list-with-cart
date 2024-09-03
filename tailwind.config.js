90758f8c4709f1331c3daec423c21d6145fc9882
jcastro <jeromec3866@gmail.com>
2024-09-03 13:43:07 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
     extend: {
-      color: {
+      colors: {
         primary_1: 'hsl(14, 86%, 42%)',
         primary_2: 'hsl(159, 69%, 38%)',
         secondary_50: 'hsl(20, 50%, 98%)',
         secondary_100: 'hsl(13, 31%, 94%)',
-        secondasry_300: 'hsl(14, 25%, 72%)',
+        secondary_300: 'hsl(14, 25%, 72%)',
         secondary_400: 'hsl(7, 20%, 60%)',
         secondary_500: 'hsl(12, 20%, 44%)',
-        secondasry_900: 'hsl(14, 65%, 9%)',
+        secondary_900: 'hsl(14, 65%, 9%)',
       },
     },
   },
