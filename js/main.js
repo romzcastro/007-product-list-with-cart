5ba0f8ec1ad5ec313025329438d9f9ea2d14a35e
jcastro <jeromec3866@gmail.com>
2024-09-06 17:23:41 +0800

    update

---
+++ js/main.js
@@ -0,0 +1,150 @@
+let carts = document.querySelectorAll('.cart-select');
+let products = [
+  {
+    image: {
+      thumbnail: './assets/images/image-waffle-thumbnail.jpg',
+      mobile: './assets/images/image-waffle-mobile.jpg',
+      tablet: './assets/images/image-waffle-tablet.jpg',
+      desktop: './assets/images/image-waffle-desktop.jpg',
+    },
+    name: 'Waffle with Berries',
+    category: 'Waffle',
+    price: 6.5,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-creme-brulee-thumbnail.jpg',
+      mobile: './assets/images/image-creme-brulee-mobile.jpg',
+      tablet: './assets/images/image-creme-brulee-tablet.jpg',
+      desktop: './assets/images/image-creme-brulee-desktop.jpg',
+    },
+    name: 'Vanilla Bean Crème Brûlée',
+    category: 'Crème Brûlée',
+    price: 7.0,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-macaron-thumbnail.jpg',
+      mobile: './assets/images/image-macaron-mobile.jpg',
+      tablet: './assets/images/image-macaron-tablet.jpg',
+      desktop: './assets/images/image-macaron-desktop.jpg',
+    },
+    name: 'Macaron Mix of Five',
+    category: 'Macaron',
+    price: 8.0,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-tiramisu-thumbnail.jpg',
+      mobile: './assets/images/image-tiramisu-mobile.jpg',
+      tablet: './assets/images/image-tiramisu-tablet.jpg',
+      desktop: './assets/images/image-tiramisu-desktop.jpg',
+    },
+    name: 'Classic Tiramisu',
+    category: 'Tiramisu',
+    price: 5.5,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-baklava-thumbnail.jpg',
+      mobile: './assets/images/image-baklava-mobile.jpg',
+      tablet: './assets/images/image-baklava-tablet.jpg',
+      desktop: './assets/images/image-baklava-desktop.jpg',
+    },
+    name: 'Pistachio Baklava',
+    category: 'Baklava',
+    price: 4.0,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-meringue-thumbnail.jpg',
+      mobile: './assets/images/image-meringue-mobile.jpg',
+      tablet: './assets/images/image-meringue-tablet.jpg',
+      desktop: './assets/images/image-meringue-desktop.jpg',
+    },
+    name: 'Lemon Meringue Pie',
+    category: 'Pie',
+    price: 5.0,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-cake-thumbnail.jpg',
+      mobile: './assets/images/image-cake-mobile.jpg',
+      tablet: './assets/images/image-cake-tablet.jpg',
+      desktop: './assets/images/image-cake-desktop.jpg',
+    },
+    name: 'Red Velvet Cake',
+    category: 'Cake',
+    price: 4.5,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-brownie-thumbnail.jpg',
+      mobile: './assets/images/image-brownie-mobile.jpg',
+      tablet: './assets/images/image-brownie-tablet.jpg',
+      desktop: './assets/images/image-brownie-desktop.jpg',
+    },
+    name: 'Salted Caramel Brownie',
+    category: 'Brownie',
+    price: 4.5,
+    inCart: 0,
+  },
+  {
+    image: {
+      thumbnail: './assets/images/image-panna-cotta-thumbnail.jpg',
+      mobile: './assets/images/image-panna-cotta-mobile.jpg',
+      tablet: './assets/images/image-panna-cotta-tablet.jpg',
+      desktop: './assets/images/image-panna-cotta-desktop.jpg',
+    },
+    name: 'Vanilla Panna Cotta',
+    category: 'Panna Cotta',
+    price: 6.5,
+    inCart: 0,
+  },
+  ,
+];
+
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener('click', () => {
+    cartNumbers(products[i]);
+  });
+}
+
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  if (productNumbers) {
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+
+function cartNumbers(products) {
+  let productNumbers = localStorage.getItem('cartNumbers');
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem('cartNumbers', productNumbers + 1);
+    document.querySelector('.cart span').textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem('cartNumbers', 1);
+    document.querySelector('.cart span').textContent = 1;
+  }
+  setItems(products);
+}
+
+function setItems(products) {
+  console.log('Inside of SetItems function');
+  console.log('My product is', products);
+  products.inCart = 1;
+  let cartItems = {
+    [products.name]: products,
+  };
+
+  localStorage.setItem('productsInCart', JSON.stringify(cartItems));
+}
+onLoadCartNumbers();
